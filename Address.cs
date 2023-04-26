c8f527ad8f9100503df40b7f2e492abc88668c44
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-26 17:42:10 +0200

    Aggiungere i file di progetto.

---
+++ Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Address
+    {
+        private string Name;
+        private string Surname;
+        private string Street;
+        private string City;
+        private string Province;
+        private string ZIP;
+    
+        public Address(string Name, string Surname, string Street, string City, string Province, string ZIP) { 
+            this.Name = Name;
+            this.Surname = Surname; 
+            this.Street = Street;
+            this.City = City;
+            this.Province = Province;
+            this.ZIP = ZIP;
+        }
+    public override string ToString()
+    {
+            return ($"Nome:{this.Name}");
+     
+    }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_lista_indirizzi;
+
+
+StreamReader file = File.OpenText("C:/Users/LombarSupport/Downloads/addresses.csv");
+
+List<Address> personAddressList = new List<Address>();
+
+int lineCounter = 0;
+string readText = file.ReadLine();
+
+while (!file.EndOfStream)
+{
+
+   
+    lineCounter++;
+}
+     if (lineCounter > 1)
+{
+    string[] splitInfo = readText.Split(',');
+
+    if (splitInfo.Length != 6)
+    {
+        Console.WriteLine($"Mancano delle informazioni nella riga {lineCounter}");
+    }
+    else
+    {
+        Address personAddress = new Address(splitInfo[0], splitInfo[1], splitInfo[2], splitInfo[3], splitInfo[4], splitInfo[5]);
+        personAddressList.Add(personAddress);
+    }
+}
+
+     
+
+
+foreach (Address personAddress in personAddressList)
+{
+    Console.WriteLine(personAddress);
+}
+
